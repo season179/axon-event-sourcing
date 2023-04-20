7e013df3700e7dc7f7365aa4cd4b35cfec7d5ae2
season <season.saw@gmail.com>
2023-04-20 21:30:28 +0800

    Added lombok and CreateOrderCommand

---
+++ src/main/java/com/axontutorial/coreapi/commands/CreateOrderCommand.java
@@ -0,0 +1,33 @@
+package com.axontutorial.coreapi.commands;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+public class CreateOrderCommand {
+
+    private final String orderId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CreateOrderCommand that = (CreateOrderCommand) o;
+        return Objects.equals(orderId, that.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId);
+    }
+
+    @Override
+    public String toString() {
+        return "CreateOrderCommand{" + "orderId='" + orderId + '\'' + '}';
+    }
+}
